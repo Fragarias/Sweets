3161af7fc9f00bb25997a0e2a46b1165cfe4e6bd
Fragarias <148366640+Fragarias@users.noreply.github.com>
2023-12-30 17:54:00 +0000

    [Add] item新規登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,3 +1,19 @@
 class Admin::ItemsController < ApplicationController
   before_action :authenticate_admin!
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :price)
+  end
 end
