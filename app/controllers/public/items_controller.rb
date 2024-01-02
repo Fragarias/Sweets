a06e24e4962d2df608b36e202739fdd40025032d
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-01-02 05:45:48 +0000

    [Add] publicItems#show view

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,4 +3,13 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all.page(params[:page]).per(8)
   end
+  def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:name, :introduction, :price)
+  end
 end
