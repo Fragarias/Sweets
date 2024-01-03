78ac0b0400e8fae7b282946f16d44d051af3c429
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-01-03 16:00:20 +0000

    [Add] publicCartItems#create機能

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,18 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def index
     @cart_item = current_customer.cart_items.all
   end
   def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
