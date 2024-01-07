b8adf3561327257a95b43c4e0ef995f2464bcc2b
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-01-07 10:53:28 +0000

    [Update] publicCartItems#create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,8 +7,17 @@ class Public::CartItemsController < ApplicationController
   end
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    @cart_item.save
+    @cart_item.customer_id = current_customer.id #追加したcart_itemのcustomer_idカラムにログインユーザーのidをいれる
+    #追加した商品がカート内に存在するかの判別
+    if CartItem.find_by(item_id: params[:cart_item][:item_id])
+      #存在する #カート内の個数をフォームから送られた個数分追加する
+      @cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      @cart_item.update(amount: @cart_item.amount)
+    else
+      #存在しない #カートモデルにレコードを新規作成する
+      @cart_item.save #追加したcart_itemを保存
+    end
     redirect_to cart_items_path
   end
   def update
