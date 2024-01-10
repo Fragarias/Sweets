abecdf3bd6f9e3ca94c426d1683a1ca6ace06384
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-01-10 15:50:25 +0000

    [Add] public#Ordershow view

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -42,6 +42,10 @@ class Public::OrdersController < ApplicationController
   def index
     @orders = current_customer.orders.all
   end
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details.all
+  end
   private
   def order_params
     params.require(:order).permit(:postal_code, :address, :name, :payment, :postage, :payment_method)
