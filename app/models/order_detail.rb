206977389908205113f8ae418e2deba828444e97
Fragarias <148366640+Fragarias@users.noreply.github.com>
2024-01-15 16:02:56 +0000

    [Add] enum定義 ステータス

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,11 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :order
   belongs_to :item
+
+  enum production_status: {
+    unable_to_start: 1,
+    waiting_for_production: 2,
+    in_production: 3,
+    production_completed: 4
+  }
 end
